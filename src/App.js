e80c3c93bd57c1fe851646a3d1d49380bbc6adbe
Bellantra <yamilapaez2@hotmail.com>
2021-08-16 04:40:00 +0200

    Limpieza general, queda acomodar cards

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ export default function App() {
      
     const apiKey = process.env.REACT_APP_API_KEY
         
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
